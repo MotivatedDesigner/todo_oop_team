e05f7374c8ab1dec598c9a356b9127b8d19ed2a6
Ayoub Elmendoub <76922728+MotivatedDesigner@users.noreply.github.com>
2021-12-14 20:39:12 +0100

    add show project functionality

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,23 +5,24 @@ import Project from "./Project.js"
 import Todo from "./Todo.js"
 
 const controller = new Controller(new UserInterface, new StorageManager);
+const addProjectModal = document.getElementById('add-project-modal')
 
 controller.showProjects()
 
-document.getElementById('add-project').addEventListener('click', () => {
-  document.getElementById('add-project-modal').classList.add('show')
-})
+document.getElementById('add-project').addEventListener('click', () => { addProjectModal.classList.add('show') })
 
 document.getElementById('project-save').addEventListener('click', () => { 
-  
   const title = document.getElementById('input-project-title').value
   const description = document.getElementById('input-project-description').value
   const dueDate = document.getElementById('input-project-dueDate').value
-  // Validation
   controller.addProject(new Project(title, description, dueDate))
-
+  addProjectModal.classList.remove('show')
 })
 
 document.getElementById('clear-projects').addEventListener('click', controller.clearProjects)
+
+document.getElementById('project-list').addEventListener('click', (event) => {
+  if(event.target.tagName == 'LI') controller.showProject(event.target.id)
+})
 
 window.onbeforeunload = () => controller.saveProjects
